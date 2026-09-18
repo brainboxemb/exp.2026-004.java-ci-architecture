4a2b47ecf4ceafa861cf1fa15babf46d97a55940
brainboxemb <285184499+brainboxemb@users.noreply.github.com>
2026-09-18 12:58:12 +0200

    Keep generic cache adapter free of product Git identity

--- candidates/maven-build-cache/prepare.sh
+++ candidates/maven-build-cache/prepare.sh
@@ -26,17 +26,6 @@ cat > .mvn/maven-build-cache-config.xml <<'EOF'
       </dirNames>
     </attachedOutputs>
   </configuration>
-  <input>
-    <plugins>
-      <plugin groupId="pl.project13.maven" artifactId="git-commit-id-plugin">
-        <dirScan mode="auto">
-          <includes>
-            <include tagName="dotGitDirectory" recursive="false" glob="HEAD"/>
-          </includes>
-        </dirScan>
-      </plugin>
-    </plugins>
-  </input>
 </cache>
 EOF
 
